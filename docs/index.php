94790a5bfed365b359558ac5d58505bfe4ba67bb
Peargee <168290492+Peargee@users.noreply.github.com>
2024-06-19 22:08:24 +0700

    Add files via upload

---
+++ docs/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<center>Nama Lengkap: ".$_POST['nama']."</center><br>";
+echo "<center>NIM: ".$_POST['NIM']."</center><br>";
+echo "<center>Tanggal Lahir: ".$_POST['tgl']."</center><br>";
+echo "<center>No.Telp: ".$_POST['tlp']."</center><br>";
+echo "<center>Email: ".$_POST['email']."</center><br>";
+?>
